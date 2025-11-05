fb6aa7a2b46a5a1e20b4ec5b2e2a66c316ada2e9
ImSandwich21 <daniel-21-silva@hotmail.com>
2025-11-05 20:34:13 +0000

    Authentication with passwords

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func main() {
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
+	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
